a4d23a5d6094b975313e21a4ead8d5b7027e0c64
Denis O <denis@universal-development.com>
2015-05-06 17:11:11 +0300

    methods for handling plaintext & html requests

--- myip-webapp/src/main/java/com/unidev/myip/HeadersController.java
+++ myip-webapp/src/main/java/com/unidev/myip/HeadersController.java
@@ -7,9 +7,12 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
+
+import static com.unidev.platform.template.TemplateBuilder.newClassPathTemplate;
 
 /**
  * Controller for returning all request headers
@@ -24,10 +27,30 @@ public class HeadersController {
     @Autowired
     private WebUtils webUtils;
 
+    @RequestMapping
+    public ModelAndView htmlRequest() {
+        JSONObject headers = buildHeadersJson();
+        ModelAndView modelAndView = new ModelAndView("headers");
+        modelAndView.addObject("headers", headers);
+        return modelAndView;
+    }
+
+    @RequestMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseBody
+    public String plainTextRequest() {
+        JSONObject headers = buildHeadersJson();
+        return newClassPathTemplate("headers.template")
+                .addVariable("headers", headers)
+                .build();
+    }
 
     @RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public JSONObject jsonRequest() {
+        return buildHeadersJson();
+    }
+
+    protected JSONObject buildHeadersJson() {
         JSONObject jsonObject = new JSONObject();
 
         Enumeration headerNames = request.getHeaderNames();

990c237e889e3b487dc15a50c0ed3b0798856657
Denis O <denis@universal-development.com>
2015-05-06 16:34:11 +0300

    empty HeadersController

---
+++ myip-webapp/src/main/java/com/unidev/myip/HeadersController.java
@@ -0,0 +1,10 @@
+package com.unidev.myip;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Controller for returning all request headers
+ */
+@Controller
+public class HeadersController {
+}
